cd118298177d90e5253ed97d4ff8bcb7a1e7b97d
Barium <nguyenminhduc12052006@gmail.com>
2025-05-09 16:19:40 +0700

    add game over screen

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -92,81 +92,137 @@ int main(int argc, char* args[]) {
 		Graphic::LTexture chosen;
 		chosen.loadFromRenderedText("You have not choose any number", { 255,255,255,255 }, gFont, gRenderer);
 		string current;
+
 		while (quit == false) {
-			if (temp == 0) {
-				chosen.loadFromRenderedText("You have not choose any number", { 255,255,255,255 }, gFont, gRenderer);
-				current = "You have not choose any number";
-			}
-			while (SDL_PollEvent(&e) != 0) {
-				if (e.type == SDL_QUIT) {
-					quit = true;
+			if (attemp == 0) {
+				Graphic::LTexture gameover;
+				gameover.LoadFromFile("metal-gear-solid-game-over-screen.png", gRenderer);
+				for (int i = 0; i < 9; i++) {
+					for (int j = 0; j < 9; j++) {
+						cells[i][j].free();
+					}
 				}
-				SDL_GetMouseState(&x, &y);
+
 				for (int i = 0; i < 3; i++) {
 					for (int j = 0; j < 3; j++) {
-						controlbutton[i][j].handleEvent(&e);
+						control[i][j].free();
 					}
 				}
-				if (insideRect(ControlGrid, x, y) == true && e.type == SDL_MOUSEBUTTONDOWN) {
-					int x1 = (x - ControlGrid.x) / (ControlGrid.w / 3);
-					int y1 = (y - (ControlGrid.y)) / (ControlGrid.h / 3);
-					cout << x1 << " " << y1 << endl;
-					temp = controlnum[y1][x1];
-					chosen.loadFromRenderedText("You have chosen the number " + to_string(temp), { 255,255,255,255 }, gFont, gRenderer);
-					cout << temp << endl;
-					current = "You have chosen the number " + to_string(temp);
-				}
-				if (insideRect(MainGrid, x, y) == true && temp != 0 && e.type == SDL_MOUSEBUTTONDOWN) {
-					int x1 = (x - MainGrid.x) / (MainGrid.w / 9);
-					int y1 = (y - MainGrid.y) / (MainGrid.h / 9);
-					cout << x1 << " " << y1 << endl;
-					if (ans[y1][x1] == temp) {
-						sudoku[y1][x1] = temp;
-						cells[y1][x1].free();
-						cells[y1][x1].loadtext(to_string(temp), gRenderer, gFont);
-						chosen.loadFromRenderedText("Correct", { 255,255,255,255 }, gFont, gRenderer);
-						current = "Correct";
+				
+				while (SDL_PollEvent(&e) != 0) {
+					if (e.type == SDL_QUIT) {
+						quit = true;
 					}
-					else {
-						attemp--;
-						chosen.loadFromRenderedText("Wrong answer", { 255,255,255,255 }, gFont, gRenderer);
-						current = "Wrong answer";
+					else if (e.type == SDL_MOUSEBUTTONDOWN) {
+						attemp = 3;
+						generate(ans);
+						int k = 30;
+						CreateSudoku(ans, sudoku, k);
+						gameover.free();
+						a.free();
+						for (int i = 0; i < 9; i++) {
+							for (int j = 0; j < 9; j++) {
+								if (sudoku[i][j] == 0) {
+									cells[i][j].loadtext(" ", gRenderer, gFont);
+									continue;
+								}
+								cells[i][j].loadtext(to_string(sudoku[i][j]), gRenderer, gFont);
+							}
+						}
+
+						for (int i = 0; i < 3; i++) {
+							for (int j = 0; j < 3; j++) {
+								control[i][j].loadtext(to_string(index), gRenderer, afont);
+								controlnum[i][j] = index;
+								index++;
+							}
+						}
 					}
-					cout << sudoku[y1][x1] << endl;
-					temp = 0;
 				}
-			}
-			SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
-			SDL_RenderClear(gRenderer);
-			
-			a.free();
-			a.transfer();
-			a.loadtime(gRenderer, gFont);
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
+				SDL_RenderClear(gRenderer);
 
-			for (int i = 0; i < 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					controlbutton[i][j].render(gRenderer);
+				gameover.render(SCREEN_WIDTH/2-gameover.getWidth()/2, 0, gRenderer);
+				a.render(SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 20, gRenderer);
+
+				SDL_RenderPresent(gRenderer);
+			}
+			else {
+				if (temp == 0) {
+					chosen.loadFromRenderedText("You have not choose any number", { 255,255,255,255 }, gFont, gRenderer);
+					current = "You have not choose any number";
 				}
-			}
+				while (SDL_PollEvent(&e) != 0) {
+					if (e.type == SDL_QUIT) {
+						quit = true;
+					}
+					SDL_GetMouseState(&x, &y);
+					for (int i = 0; i < 3; i++) {
+						for (int j = 0; j < 3; j++) {
+							controlbutton[i][j].handleEvent(&e);
+						}
+					}
+					if (insideRect(ControlGrid, x, y) == true && e.type == SDL_MOUSEBUTTONDOWN) {
+						int x1 = (x - ControlGrid.x) / (ControlGrid.w / 3);
+						int y1 = (y - (ControlGrid.y)) / (ControlGrid.h / 3);
+						cout << x1 << " " << y1 << endl;
+						temp = controlnum[y1][x1];
+						chosen.loadFromRenderedText("You have chosen the number " + to_string(temp), { 255,255,255,255 }, gFont, gRenderer);
+						cout << temp << endl;
+						current = "You have chosen the number " + to_string(temp);
+					}
+					if (insideRect(MainGrid, x, y) == true && temp != 0 && e.type == SDL_MOUSEBUTTONDOWN) {
+						int x1 = (x - MainGrid.x) / (MainGrid.w / 9);
+						int y1 = (y - MainGrid.y) / (MainGrid.h / 9);
+						cout << x1 << " " << y1 << endl;
+						if (ans[y1][x1] == temp) {
+							sudoku[y1][x1] = temp;
+							cells[y1][x1].free();
+							cells[y1][x1].loadtext(to_string(temp), gRenderer, gFont);
+							chosen.loadFromRenderedText("Correct", { 255,255,255,255 }, gFont, gRenderer);
+							current = "Correct";
+						}
+						else {
+							attemp--;
+							chosen.loadFromRenderedText("Wrong answer", { 255,255,255,255 }, gFont, gRenderer);
+							current = "Wrong answer";
+						}
+						cout << sudoku[y1][x1] << endl;
+						temp = 0;
+					}
+				}
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0x00);
+				SDL_RenderClear(gRenderer);
 
-			GridLine(495, gRenderer, SCREEN_WIDTH*10/20, SCREEN_HEIGHT/8);
-			GridLine(330, gRenderer, SCREEN_WIDTH * 3 / 20, SCREEN_HEIGHT / 5, 3);
+				a.free();
+				a.transfer();
+				a.loadtime(gRenderer, gFont);
 
-			rendernumber(cells, SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 8, 495, gRenderer);
-			rendernumber(control, SCREEN_WIDTH * 3 / 20, SCREEN_HEIGHT / 5, 330, gRenderer, 3);
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						controlbutton[i][j].render(gRenderer);
+					}
+				}
 
-			chosen.render(0, SCREEN_HEIGHT * 2 / 20, gRenderer);
+				GridLine(495, gRenderer, SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 8);
+				GridLine(330, gRenderer, SCREEN_WIDTH * 3 / 20, SCREEN_HEIGHT / 5, 3);
 
-			a.render(SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 20, gRenderer);
+				rendernumber(cells, SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 8, 495, gRenderer);
+				rendernumber(control, SCREEN_WIDTH * 3 / 20, SCREEN_HEIGHT / 5, 330, gRenderer, 3);
 
-			SDL_RenderPresent(gRenderer);
+				chosen.render(0, SCREEN_HEIGHT * 2 / 20, gRenderer);
 
-			if (current == "Correct") {
-				SDL_Delay(500);
-			}
+				a.render(SCREEN_WIDTH * 10 / 20, SCREEN_HEIGHT / 20, gRenderer);
 
-			if (current == "Wrong answer") {
-				SDL_Delay(500);
+				SDL_RenderPresent(gRenderer);
+
+				if (current == "Correct") {
+					SDL_Delay(500);
+				}
+
+				if (current == "Wrong answer") {
+					SDL_Delay(500);
+				}
 			}
 		}
 	}
